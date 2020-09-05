2051088edb92d0802949c58d175cb8dbaa589e3a
Soumotanu Mazumdar <msoumotanu@gmail.com>
2020-09-05 17:47:09 +0530

    nav bar color change

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+const navLinks = document.querySelectorAll('.nav-link');
+const navbar = document.querySelector('#navbar');
+const navAnchor = document.querySelectorAll('.nav-links a')
+
 const navSlide = () => {
-	const burger = document.querySelector('.burger');
-	const nav = document.querySelector('.nav-links');
-	const navLinks = document.querySelectorAll('.nav-link');
 
 	burger.addEventListener('click', () => {
 		nav.classList.toggle('nav-active');
@@ -15,6 +18,31 @@ const navSlide = () => {
 			}
 		});
 	});
+
+
+window.onscroll = () => {
+	console.log(this.scrollY, window.innerHeight)
+	if (this.scrollY >= window.innerHeight * 2.92)
+		navbar.style.background = `#183E55`;
+	else if (this.scrollY >= window.innerHeight * 1.92){
+		navbar.style.background = `#CFD752`;
+	}
+  	else if (this.scrollY >= window.innerHeight / 10)
+  		navbar.style.background = `#5C7237`;
+  	else
+  		navbar.style.background = `transparent`;
+  	if (this.scrollY >= window.innerHeight * 1.92 && this.scrollY <= window.innerHeight * 2.92){
+  		navAnchor.forEach((i, index) => {
+			i.style.color = `black`;
+		});
+	}
+	else {
+		navAnchor.forEach((i, index) => {
+			i.style.color = `white`;
+		});
+	}
 };
+};
+
 
 navSlide();
